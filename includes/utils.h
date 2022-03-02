3c60f372e3531e72cc42be94752f77924842e455
ajearuth <ajearuth@student.42.fr>
2022-03-02 19:06:46 +0100

    add color codes, returns, prompt

---
+++ includes/utils.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 18:56:39 by ajearuth          #+#    #+#             */
+/*   Updated: 2022/03/02 19:05:53 by ajearuth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+//returns 
+# define SUCCESS 0
+# define FAILURE -1
+
+//colors 
+# define GREEN "\033[0;32m"
+# define PURPLE "\033[0;35m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[0;36m"
+# define RED "\033[4;31m"
+# define RESET "\033[0m"
+
+//prompt 
+# define PROMPT "Minishell 🌌 >"
+
+#endif

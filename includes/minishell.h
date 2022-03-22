01b315cd12a6ef0112c35162f598dc56c660dded
Anna JEARUTH <ajearuth@student.42.fr>
2022-03-22 12:15:50 +0100

    include normed

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 18:19:05 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/03/22 11:12:44 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/03/22 12:15:15 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,8 @@ typedef struct s_data
 }	t_data;
 
 	/* prompt.c */
-int	print_prompt(t_data *data);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		print_prompt(t_data *data);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 	/* init.c */
 void	init_token(t_token *token);
@@ -51,49 +51,45 @@ void	init_data(t_data *data);
 
 	/* parse.c */
 t_token	*parse(t_token *list, t_data *data);
-char* ft_strtrim(const char *s1, const char *set);
+char	*ft_strtrim(const char *s1, const char *set);
 t_token	*check_category(t_token *list, t_data *data);
 
 	/* protection_quote.c */
-int check_quotes(char *token);
-int	unclose_quote(char *str);
-
-	/* check_parse.c */
-int	checker_red(t_token *token);
+int		check_quotes(char *token);
+int		unclose_quote(char *str);
 
 	/* utils.c */
-int ft_strlen_word(char *str);
-int ft_strlen_dollar(char *str);
-int	ft_strlen_between_quotes(char *str);
-int check_sep_for_word(int c);
-int go_for_word(int c);
+int		ft_strlen_word(char *str);
+int		ft_strlen_dollar(char *str);
+int		ft_strlen_between_quotes(char *str);
+int		check_sep_for_word(int c);
+int		go_for_word(int c);
 
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 
-	/* category */ 
-t_token *r_redirect(t_token *list, t_data *data);
-t_token *l_redirect(t_token *list, t_data *data);
-t_token *token_word(t_token *list, t_data *data);
+	/* token_others.c */
+t_token	*r_redirect(t_token *list, t_data *data);
+t_token	*l_redirect(t_token *list, t_data *data);
+t_token	*token_word(t_token *list, t_data *data);
 t_token	*token_dollar(t_token *list, t_data *data);
 
-/* check-_spaces */
-
-int	is_quote_odd(t_token *list, t_data *data);
-int	count_quote(char *str, char quote);
-t_token *token_between_dquote(t_token *list, t_data *data);
+	/* token_quote.c */
+int		count_quote(char *str, char quote);
+t_token	*token_between_dquote(t_token *list, t_data *data);
 t_token	*token_between_squote(t_token *list, t_data *data);
 
 	/* token_list.c */
-t_token *add_list(t_token *list, t_token *new_token);
-t_token *create_node(t_token *list, char *str, int category);
+t_token	*add_list(t_token *list, t_token *new_token);
+t_token	*create_node(t_token *list, char *str, int category);
 
 	/* is_it_available.c */
-int	is_valid_left(t_token *list);
-int	is_valid_right(t_token *list);
-int	is_valid_t_pipe(t_token *list);
+int		checker_red(t_token *token);
+int		is_valid_left(t_token *list);
+int		is_valid_right(t_token *list);
+int		is_valid_t_pipe(t_token *list);
 
 	/* free.c */
 void	free_list(t_token **list);
